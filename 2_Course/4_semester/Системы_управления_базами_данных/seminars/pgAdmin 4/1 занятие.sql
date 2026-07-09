4fdc6dca7f326ac2b11907a5ebc51a274e5a2517
Chentsov <115504184+EgorChen246@users.noreply.github.com>
2026-07-09 12:56:25 +0300

    Delete 2_Course/4_semester/Системы_управления_базами_данных/seminars/pgAdmin 4 directory

--- 2_Course/4_semester/Системы_управления_базами_данных/seminars/pgAdmin 4/1 занятие.sql
+++
@@ -1,6 +0,0 @@
---dROP TABLE customers;
-
-Create table if not exists customers(id SERIAL PRIMARY KEY, Age INT, Email Character varying(30));
-
-insert into customers values(3,22, 'baz@bar.com');
-Select * from customers;
